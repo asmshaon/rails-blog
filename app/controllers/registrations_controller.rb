3307519d072df84ab052fc24814e388ad9065de3
Abu Saleh Muhammad Shaon <srabon.php@gmail.com>
2015-02-04 19:05:18 +0600

    Devise Custom SignIn/SignUp Added

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,12 +5,22 @@ class RegistrationsController < Devise::RegistrationsController
   def create
     build_resource(sign_up_params)
 
-    if resource.save
-      sign_up(resource_name, resource)
-      return render :json => {:success => true}
+    if request.xhr?
+      if resource.save
+        sign_up(resource_name, resource)
+        return render :json => {:success => true}
+      else
+        clean_up_passwords resource
+        return render :json => {:success => false, :errors => resource.errors.full_messages.join('<br/>').html_safe}
+      end
     else
-      clean_up_passwords resource
-      return render :json => {:success => false,  :errors => resource.errors.full_messages.join('<br/>').html_safe}
+      if resource.save
+        set_flash_message :notice, :signed_up if is_flashing_format?
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        redirect_to '/users/sign_up', :flash => { :error => resource.errors.full_messages.join('<br/>').html_safe }
+      end
     end
   end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,19 @@ class SessionsController < Devise::SessionsController
   respond_to :json
 
   def create
-    if user_signed_in?
-        render :json => {:success => true}
+    if request.xhr?
+      if user_signed_in?
+        return render :json => {:success => true}
+      else
+        return render :json => {:success => false, :errors => (I18n.t 'devise.failure.invalid')}
+      end
     else
-      return render :json => {:success => false, :errors => (I18n.t 'devise.failure.invalid') }
+      if user_signed_in?
+        set_flash_message(:notice, :signed_in) if is_flashing_format?
+        redirect_to root_path
+      else
+        redirect_to '/users/sign_in', :flash => { :error => (I18n.t 'devise.failure.invalid') }
+      end
     end
   end
 
